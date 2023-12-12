88fb076e804ade25826ec381e050f837937ac34e
thomssonkarl <thomsson_99@hotmail.com>
2023-12-12 16:06:05 +0100

    Day 7 p1 done

---
+++ 7.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <sstream>
+
+struct Hand {
+    std::string str;
+    uint16_t type;
+    int bid;
+};
+
+bool cgt(char rank1, char rank2) {
+    std::string card_order = "23456789TJQKA";
+    size_t p1 = card_order.find(rank1);
+    size_t p2 = card_order.find(rank2);
+    return p1 > p2;
+}
+
+uint16_t check_hand(std::string hand) {
+    std::map<char, uint16_t> c_count;
+    for (char c : hand) c_count[c]++;
+
+    std::vector<uint16_t> values;
+    for (const auto& pair : c_count) values.push_back(pair.second);
+    std::sort(values.begin(), values.end());
+
+    // Handle full house / four of a kind
+    if(values.size() == 2) return (values[0] == 2) ? 5 : 6; 
+
+    // Handle 2 pair / three of a kind
+    if(values.size() == 3) return (values[2] == 3) ? 4 : 3;
+
+    if(values.size() == 5) return 1; // [1,1,1,1,1] 
+    if(values.size() == 4) return 2; // [1,1,1,2]
+    if(values.size() == 1) return 7; // [5]
+
+    return 0;
+}
+
+int compare_hands(std::string h1, std::string h2) {
+    if (check_hand(h1) > check_hand(h2)) return -1;
+    if (check_hand(h2) > check_hand(h1)) return 1;
+    for(size_t c = 0; c < 5; c++) {
+        if (cgt(h1[c], h2[c])) return -1;
+        if (cgt(h2[c], h1[c])) return 1;
+    } 
+    return 0;
+}
+
+void sort_hands(std::vector<Hand>& arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (compare_hands(arr[j].str, arr[j + 1].str) < 0) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+
+int solve(const std::vector<std::string>& input) {
+    uint32_t result{}; 
+
+    std::vector<Hand> hands;
+
+    for (const auto& str : input) {
+        std::stringstream ss(str);
+        std::string hand, ba;
+        ss >> hand >> ba;
+        hands.push_back({hand, check_hand(hand), std::stoi(ba)});
+    }
+
+    sort_hands(hands);
+    
+    for (size_t i = 0; i < hands.size(); ++i) result += hands[i].bid * (i+1);
+    
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+
+    }
+
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening the input file!" << std::endl;
+        return 1;
+
+    }
+
+    std::vector<std::string> input;
+    std::string line;
+
+    while (getline(inputFile, line)) {
+        input.push_back(line);
+
+    }
+
+    inputFile.close();
+
+    int result = solve(input);
+
+    std::cout << "Solution: " << result << std::endl;
+
+    return 0;
+
+}
+
