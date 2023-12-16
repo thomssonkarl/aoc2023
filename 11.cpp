9c98cc60c3dac4b3cc00607bee2d287eabd01a68
thomssonkarl <thomsson_99@hotmail.com>
2023-12-16 10:42:18 +0100

    Works but is too slow

--- 11.cpp
+++ 11.cpp
@@ -6,27 +6,26 @@
 #include <map>
 #include <sstream>
 
-int solve(const std::vector<std::string>& input) {
-    uint32_t result{}; 
+uint64_t solve(const std::vector<std::string>& input, size_t expansion_factor) {
+    uint64_t result{}; 
     
     size_t key = 1; 
     std::map<size_t, std::pair<int, int>> coords;
-    size_t y = 0;
-    for (auto& str : input) {
+    for (size_t y = 0; y < input.size(); ++y) {
         std::vector<size_t> xs;
-        size_t hashtag = str.find('#');
+        size_t hashtag = input[y].find('#');
         while (hashtag != std::string::npos) {
             xs.push_back(hashtag);
-            hashtag = str.find('#', hashtag + 1);
+            hashtag = input[y].find('#', hashtag + 1);
         }
-
         if (!xs.empty()) {
             for (auto x : xs) {
                 coords[key] = std::make_pair(y, x); 
                 ++key;
             }
+        } else {
+            y += (expansion_factor - 1);
         }
-       ++y; 
     }
 
     std::vector<std::pair<size_t, size_t>> key_pairs;
@@ -48,34 +47,18 @@ int solve(const std::vector<std::string>& input) {
     return result;
 }
 
-void expand_galaxies(std::vector<std::string>& input) {
+void expand_galaxies(std::vector<std::string>& input, size_t expansion_factor) {
     
-    std::vector<size_t> to_expand;
     size_t size = input.size();
 
-    for (size_t r = 0; r < size; ++r) {
-        bool expand = true;
-        for (size_t c = 0; c < size; ++c) {
-            if (input[r][c] != '.') {
-                expand = false;
-                break;  
-            }
-        }
-        if(expand) {
-            to_expand.push_back(r); 
-        }
+    for (size_t r = 0; r < input.size(); ++r) {
+        if (input[r].find("#") != std::string::npos) continue;  
+        input.insert(input.begin() + (r+1), expansion_factor-1, std::string(size, '.'));
+        r += (expansion_factor-1);
     }
-
-    size_t offset = 0;
-    for (size_t row : to_expand) {
-        input.insert(input.begin() + (row+1+offset), std::string(size, '.'));
-        ++offset;
-    }
-
-    to_expand.clear();
     size = input.size();
 
-    for (size_t c = 0; c < size; ++c) {
+    for (size_t c = 0; c < input[0].size(); ++c) {
         bool expand = true;
         for(size_t r = 0; r < size; ++r) {
             if(input[r][c] != '.') {
@@ -84,33 +67,30 @@ void expand_galaxies(std::vector<std::string>& input) {
             }
         }
         if (expand) {
-            to_expand.insert(to_expand.begin(), c);
-        }
-            
-    }
-
-    for (size_t col : to_expand) {
-        for(size_t i = 0; i < input.size(); ++i) {
-            if (i==0) input[i].insert(input[i].begin() + col, '.'); 
-            else input[i].insert(input[i].begin() + col + 1, '.'); 
+            std::string expansion((expansion_factor-1), '.');
+            for (size_t i = 0; i < input.size(); ++i) {
+                input[i].insert(c, expansion);  
+            }
+            c += (expansion_factor-1);
         }
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
         return 1;
 
     }
 
     std::ifstream inputFile(argv[1]);
+    bool part2 = std::stoi(argv[2]);
     if (!inputFile.is_open()) {
         std::cerr << "Error opening the input file!" << std::endl;
         return 1;
 
     }
-
+    
     std::vector<std::string> input;
     std::string line;
     while (getline(inputFile, line)) {
@@ -118,11 +98,12 @@ int main(int argc, char *argv[]) {
     }
 
     inputFile.close();
+    size_t expansion_factor = part2 ? 1000000 : 2;
 
-    expand_galaxies(input);
-
-    int result = solve(input);
-
+    expand_galaxies(input, expansion_factor);
+    
+    
+    uint64_t result = solve(input, expansion_factor);
     std::cout << "Solution: " << result << std::endl;
 
     return 0;
