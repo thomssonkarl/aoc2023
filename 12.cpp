78c2f78e06fe51014f3b6de4d7e1c804045da534
thomssonkarl <thomsson_99@hotmail.com>
2023-12-18 11:46:27 +0100

    Day 12 part 2 done

--- 12.cpp
+++ 12.cpp
@@ -8,9 +8,16 @@ bool has_hashtag(const std::string& str) {
     return str.find("#") != std::string::npos;
 }
 
-size_t arrangements(const std::string& str, const std::vector<uint16_t>& groups) {
+using cachekey_t = std::pair<std::string, std::vector<uint16_t>>;
 
-    size_t result = 0;    
+std::map<cachekey_t, size_t> cache;
+
+uint64_t arrangements(const std::string& str, const std::vector<uint16_t>& groups) {
+
+    uint64_t result = 0;    
+    cachekey_t cache_key = std::make_pair(str, groups);
+    auto cached = cache.find(cache_key);
+    if(cached != cache.end()) return cached->second;
 
     if (groups.empty()) {
         return has_hashtag(str) ? 0 : 1;
@@ -34,12 +41,51 @@ size_t arrangements(const std::string& str, const std::vector<uint16_t>& groups)
             }
         }
     }
+    cache[cache_key] = result;
     return result;
 }
 
+std::string transform_string(const std::string& input) {
+    std::istringstream iss(input);
+    std::string conditions;
+    std::string groups;
 
-int solve(const std::vector<std::string>& input) {
-    uint32_t result{}; 
+    iss >> conditions >> groups;
+
+    std::string transformed_conditions;
+    for (int i = 0; i < 5; ++i) {
+        if (!transformed_conditions.empty()) {
+            transformed_conditions += '?';
+        }
+        transformed_conditions += conditions;
+    }
+
+    std::istringstream groups_stream(groups);
+    std::vector<std::string> groups_list;
+    std::copy(std::istream_iterator<std::string>(groups_stream),
+              std::istream_iterator<std::string>(),
+              std::back_inserter(groups_list));
+
+    std::string transformed_groups;
+    for (int i = 0; i < 5; ++i) {
+        if (!transformed_groups.empty()) {
+            transformed_groups += ',';
+        }
+        for (const auto& group : groups_list) {
+            transformed_groups += group;
+            transformed_groups += ',';
+        }
+        transformed_groups.pop_back();
+    }
+
+    std::string transformed_string = transformed_conditions + " " + transformed_groups;
+
+    return transformed_string;
+}
+
+
+uint64_t solve(const std::vector<std::string>& input) {
+    uint64_t result{}; 
 
     for (const auto& str : input) {
         size_t space = str.find_first_of(' ');
@@ -57,12 +103,12 @@ int solve(const std::vector<std::string>& input) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << " <?part2>" << std::endl;
         return 1;
 
     }
-
+    bool part2 = std::stoi(argv[2]);
     std::ifstream inputFile(argv[1]);
     if (!inputFile.is_open()) {
         std::cerr << "Error opening the input file!" << std::endl;
@@ -73,12 +119,12 @@ int main(int argc, char *argv[]) {
     std::vector<std::string> input;
     std::string line;
     while (getline(inputFile, line)) {
-        input.push_back(line);
+        input.push_back(part2 ? transform_string(line) : line);
     }
 
     inputFile.close();
 
-    int result = solve(input);
+    uint64_t result = solve(input);
 
     std::cout << "Solution: " << result << std::endl;
 
